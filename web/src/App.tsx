5e09a175c21d229979e92bcda664c1a3be72ba44
Logan Thimer <thimerlogan@gmail.com>
2023-12-20 09:12:12 -0700

    Added basic side navigation

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import "./App.css";
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Planner from "./pages/Planner";
+
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline text-red-600">
-        Simple React Typescript Tailwind Sample
-      </h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Planner />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
