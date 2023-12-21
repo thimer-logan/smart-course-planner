9eb7753fae57e194bdc87b71659690580d8188e7
Logan Thimer <thimerlogan@gmail.com>
2023-12-21 10:19:30 -0700

    Added hex colors

--- web/src/components/constants/courseColors.ts
+++ web/src/components/constants/courseColors.ts
@@ -19,4 +19,25 @@ const courseColors: string[] = [
   "bg-rose-300",
 ];
 
+export const courseHexColors: string[] = [
+  "#d6d3d1", // stone-300
+  "#fca5a5", // red-300
+  "#fdba74", // orange-300
+  "#fcd34d", // amber-300
+  "#fde047", // yellow-300
+  "#bef264", // lime-300
+  "#86efac", // green-300
+  "#6ee7b7", // emerald-300
+  "#5eead4", // teal-300
+  "#67e8f9", // cyan-300
+  "#7dd3fc", // sky-300
+  "#93c5fd", // blue-300
+  "#a5b4fc", // indigo-300
+  "#c4b5fd", // violet-300
+  "#d8b4fe", // purple-300
+  "#f0abfc", // fuchsia-300
+  "#f9a8d4", // pink-300
+  "#fda4af", // rose-300
+];
+
 export default courseColors;
