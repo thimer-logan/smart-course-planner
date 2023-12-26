d955f50167ca67f55a4430a930c432650c1718fb
Logan Thimer <thimerlogan@gmail.com>
2023-12-26 10:14:23 -0700

    Modified timeslots so that they are displayed in order

--- web/src/components/course/CourseSectionItem.tsx
+++ web/src/components/course/CourseSectionItem.tsx
@@ -1,20 +1,22 @@
 import React from "react";
 import { CourseSection } from "../../types/Course";
 import { DayOfWeek } from "../../types/Timeslot";
+import { sortTimeslotsByDay } from "../../utils/courseUtils";
 
 interface CourseSectionItemProps {
   section: CourseSection;
 }
 
 const CourseSectionItem = ({ section }: CourseSectionItemProps) => {
-  const allSame = section.timeslots.every(
+  const sortedTimeslots = sortTimeslotsByDay(section.timeslots);
+  const allSame = sortedTimeslots.every(
     (slot, _, arr) =>
       slot.startTime === arr[0].startTime && slot.endTime === arr[0].endTime
   );
 
   let timeslots = (
     <div className="text-sm my-auto">
-      {section.timeslots.map((slot, index) => (
+      {sortedTimeslots.map((slot, index) => (
         <div key={index}>
           {`${
             slot.dayOfWeek === DayOfWeek.Thursday
@@ -27,7 +29,7 @@ const CourseSectionItem = ({ section }: CourseSectionItemProps) => {
   );
 
   if (allSame) {
-    const days = section.timeslots
+    const days = sortedTimeslots
       .map((slot) =>
         slot.dayOfWeek === DayOfWeek.Thursday ? "R" : slot.dayOfWeek.charAt(0)
       )
@@ -35,7 +37,7 @@ const CourseSectionItem = ({ section }: CourseSectionItemProps) => {
 
     timeslots = (
       <div className="text-sm my-auto">
-        {`${days} ${section.timeslots[0].startTime} - ${section.timeslots[0].endTime}`}
+        {`${days} ${sortedTimeslots[0].startTime} - ${sortedTimeslots[0].endTime}`}
       </div>
     );
   }
