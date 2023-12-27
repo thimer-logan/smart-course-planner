71fc64d92c1be310388245ecf35e0dfc110cd274
Logan Thimer <thimerlogan@gmail.com>
2023-12-27 10:43:02 -0700

    Integrated component with CourseMenu

--- web/src/pages/Planner.tsx
+++ web/src/pages/Planner.tsx
@@ -1,20 +1,34 @@
 import React, { useMemo } from "react";
 import CourseMenu from "../components/course/CourseMenu";
 import Schedule from "../components/schedule/Schedule";
-import { useAppSelector } from "../redux/hooks";
+import { useAppDispatch, useAppSelector } from "../redux/hooks";
 import { courseToCourseEvent } from "../utils/courseUtils";
 import Course from "../types/Course";
+import { addCourse, removeCourse } from "../redux/plannerSlice";
 
 const Planner = () => {
-  const courses = useAppSelector((state) => state.courses.items);
+  const courses = useAppSelector((state) => state.planner.courses);
+  const dispatch = useAppDispatch();
   const events = useMemo(
     () => courses.flatMap((course: Course) => courseToCourseEvent(course)),
     [courses]
   );
 
+  const courseAddedHandler = (course: Course) => {
+    dispatch(addCourse(course));
+  };
+
+  const courseRemovedHandler = (course: Course) => {
+    dispatch(removeCourse(course.id));
+  };
+
   return (
     <>
-      <CourseMenu courses={courses} />
+      <CourseMenu
+        courses={courses}
+        onCourseAdded={courseAddedHandler}
+        onCourseRemoved={courseRemovedHandler}
+      />
       <Schedule events={events} />
     </>
   );
