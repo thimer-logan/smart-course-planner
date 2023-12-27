a7bb94856231bef800ee1ef15d629845fdd87468
Logan Thimer <thimerlogan@gmail.com>
2023-12-27 09:43:17 -0700

    Added validation and state management to AddCourseForm

--- web/src/components/planner/AddCourseForm.tsx
+++ web/src/components/planner/AddCourseForm.tsx
@@ -1,19 +1,62 @@
 import { useState } from "react";
 import AddSectionForm from "./AddSectionForm";
-import Timeslot from "../../types/Timeslot";
-import { CourseSection } from "../../types/Course";
+import Course, { CourseSection } from "../../types/Course";
 import CourseSectionList from "../course/CourseSectionList";
+import { Input } from "antd";
+import ColorPicker from "../ui/ColorPicker";
+import { courseHexColors } from "../../constants/courseColors";
 
-const AddCourseForm = () => {
+interface AddCourseFormProps {
+  onSubmit: (course: Course) => void;
+  onCancel: () => void;
+  defaultValue?: Course;
+}
+
+const AddCourseForm = ({
+  onSubmit,
+  onCancel,
+  defaultValue,
+}: AddCourseFormProps) => {
   const [addSectionVisible, setAddSectionVisible] = useState<boolean>(false);
-  const [sections, setSections] = useState<CourseSection[]>([]);
+  const [course, setCourse] = useState<Course>({
+    id: defaultValue?.id || "",
+    name: defaultValue?.name || "",
+    sections: defaultValue?.sections || [],
+    color: defaultValue?.color || "",
+  });
+  const [errors, setErrors] = useState({
+    id: "",
+    name: "",
+    color: "",
+    sections: "",
+  });
+
+  console.log(course);
+  console.log(defaultValue);
+
+  const inputStyles = "hover:border-bittersweet focus:border-bittersweet";
+
+  const updateField = (field: string, value: string | CourseSection[]) => {
+    setCourse((prevCourse) => ({ ...prevCourse, [field]: value }));
+  };
+
+  // Validation functions
+  const validateField = {
+    id: () => course.id.trim() !== "",
+    name: () => course.name.trim() !== "",
+    color: () => course.color.trim() !== "",
+    sections: () => course.sections.length > 0,
+  };
 
   const sectionRemoveHandler = (name: string) => {
-    setSections((prev) => prev.filter((e) => e.name !== name));
+    updateField(
+      "sections",
+      course.sections.filter((e) => e.name !== name)
+    );
   };
 
   const sectionAddedHandler = (section: CourseSection) => {
-    setSections((prev) => [...prev, section]);
+    updateField("sections", [...course.sections, section]);
     setAddSectionVisible(false);
   };
 
@@ -21,9 +64,68 @@ const AddCourseForm = () => {
     setAddSectionVisible(false);
   };
 
+  const colorSelectedHandler = (color: string) => {
+    updateField("color", color === course.color ? "" : color);
+  };
+
+  const inputChangedHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+    updateField(id, value);
+  };
+
+  const submitHandler = () => {
+    const isValid = Object.entries(validateField).reduce(
+      (valid, [field, validate]) => {
+        const isFieldValid = validate();
+
+        setErrors((prevErrors) => ({
+          ...prevErrors,
+          [field]: isFieldValid ? "" : `${field} is required`,
+        }));
+
+        return valid && isFieldValid;
+      },
+      true
+    );
+
+    if (isValid) {
+      onSubmit(course);
+    }
+  };
+
   return (
     <div className="flex flex-col gap-1">
       <h2 className="font-bold text-lg">Add new course</h2>
+      <div className="flex flex-row justify-between gap-3 my-2">
+        <Input
+          id="id"
+          type="text"
+          className={`${inputStyles}`}
+          value={course.id}
+          onChange={inputChangedHandler}
+          placeholder="Course ID"
+          autoComplete="off"
+          status={`${errors.id && "error"}`}
+        />
+
+        <Input
+          id="name"
+          type="text"
+          className={`${inputStyles}`}
+          value={course.name}
+          onChange={inputChangedHandler}
+          placeholder="Course Name"
+          autoComplete="off"
+          status={`${errors.name && "error"}`}
+        />
+      </div>
+      <div className="pb-3">
+        <ColorPicker
+          colors={courseHexColors}
+          selectedColor={course.color}
+          onColorSelect={colorSelectedHandler}
+        />
+      </div>
       <div>
         <button
           onClick={() => setAddSectionVisible(true)}
@@ -34,7 +136,7 @@ const AddCourseForm = () => {
       </div>
       <div className="px-6">
         <CourseSectionList
-          sections={sections}
+          sections={course.sections}
           onRemove={sectionRemoveHandler}
         />
       </div>
@@ -45,6 +147,32 @@ const AddCourseForm = () => {
           onCancel={sectionAddCancelHandler}
         />
       )}
+
+      {Object.entries(errors).map(
+        ([field, error]) =>
+          error &&
+          field !== "id" &&
+          field !== "name" && (
+            <p key={field} className="text-red-500">
+              {error}
+            </p>
+          )
+      )}
+
+      <div className="flex flex-row justify-end gap-1 mt-4">
+        <button
+          className="bg-transparent text-bittersweet text-s px-4 py-1 rounded-lg shadow-lg hover:text-bittersweet-700 transition duration-300"
+          onClick={onCancel}
+        >
+          Cancel
+        </button>
+        <button
+          className="bg-bittersweet text-white text-s px-4 py-1 rounded-lg shadow-lg hover:bg-bittersweet-700 transition duration-300"
+          onClick={submitHandler}
+        >
+          Confirm
+        </button>
+      </div>
     </div>
   );
 };
