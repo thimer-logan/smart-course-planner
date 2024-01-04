cf35fec82254de0905834929cd924e4225caab01
Logan Thimer <thimerlogan@gmail.com>
2024-01-04 09:36:47 -0700

    Added basic layout for home screen

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { useMemo } from "react";
 import CourseMenu from "../components/course/CourseMenu";
+import ScheduleLayout from "../components/schedule/ScheduleLayout";
+import { useAppDispatch, useAppSelector } from "../redux/hooks";
+import Course from "../types/Course";
+import { addCourse, removeCourse, updateCourse } from "../redux/coursesSlice";
+import { createSchedule } from "../utils/scheduleUtils";
 
 const Home = () => {
-  return <></>;
+  const courses = useAppSelector((state) => state.courses.items);
+  const schedule = useMemo(() => createSchedule(courses), [courses]);
+  const dispatch = useAppDispatch();
+
+  const courseAddedHandler = (course: Course) => {
+    dispatch(addCourse(course));
+  };
+
+  const courseChangedHandler = (course: Course) => {
+    dispatch(updateCourse(course));
+  };
+
+  const courseRemovedHandler = (course: Course) => {
+    dispatch(removeCourse(course.id));
+  };
+
+  return (
+    <>
+      <div className="flex flex-col">
+        <CourseMenu
+          courses={courses}
+          onCourseAdded={courseAddedHandler}
+          onCourseChanged={courseChangedHandler}
+          onCourseRemoved={courseRemovedHandler}
+        />
+      </div>
+      <div className="flex flex-row flex-grow">
+        <ScheduleLayout schedules={[schedule]} />
+      </div>
+    </>
+  );
 };
 
 export default Home;
