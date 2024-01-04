44b3162f3ccd683095c2dc93baf30afd2de2e672
Logan Thimer <thimerlogan@gmail.com>
2024-01-04 09:14:03 -0700

    Bug fix with stringifying the courses

--- web/src/utils/apiUtils.ts
+++ web/src/utils/apiUtils.ts
@@ -1,7 +1,10 @@
-import Course from "../types/Course";
+import Course, { CourseSection, CourseSectionType } from "../types/Course";
+import Timeslot from "../types/Timeslot";
 
 export const fetchPlans = async (courses: Course[]) => {
   const coursesJson = stringifyCourses(courses);
+  console.log(coursesJson);
+
   const response = await fetch("http://localhost:8080/schedule/create", {
     method: "POST",
     headers: {
@@ -15,13 +18,60 @@ export const fetchPlans = async (courses: Course[]) => {
 
 export const stringifyCourses = (courses: Course[]): string => {
   const transformedCourses = courses.map((course) => {
-    const timeslots = course.sections.map((section) => section.timeslots);
+    const lectures: CourseSection[] = [],
+      labs: CourseSection[] = [],
+      seminars: CourseSection[] = [];
 
-    // Create the transformed course object
+    // Single pass to categorize sections
+    course.sections.forEach((section) => {
+      switch (section.type) {
+        case CourseSectionType.Lecture:
+          lectures.push(section);
+          break;
+        case CourseSectionType.Lab:
+          labs.push(section);
+          break;
+        case CourseSectionType.Seminar:
+          seminars.push(section);
+          break;
+      }
+    });
+
+    const allCombinations: CourseSection[][] = [];
+
+    const generateCombinations = (
+      lectures: CourseSection[],
+      labs: CourseSection[],
+      seminars: CourseSection[],
+      combination: CourseSection[] = []
+    ) => {
+      if (lectures.length > 0) {
+        lectures.forEach((lecture) =>
+          generateCombinations([], labs, seminars, combination.concat(lecture))
+        );
+      } else if (labs.length > 0) {
+        labs.forEach((lab) =>
+          generateCombinations([], [], seminars, combination.concat(lab))
+        );
+      } else if (seminars.length > 0) {
+        seminars.forEach((seminar) =>
+          generateCombinations([], [], [], combination.concat(seminar))
+        );
+      } else {
+        allCombinations.push(combination);
+      }
+    };
+
+    generateCombinations(lectures, labs, seminars);
+
+    const timeslotCombinations: Timeslot[][] = allCombinations.map(
+      (combination) => combination.flatMap((section) => section.timeslots)
+    );
+
     const transformedCourse = {
       id: course.id,
       name: course.name,
-      timeslots: timeslots,
+      timeslots: timeslotCombinations,
     };
 
     return transformedCourse;
