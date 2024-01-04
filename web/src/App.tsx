444d107e4fd333f9df435c2975d8615684339ac6
Logan Thimer <thimerlogan@gmail.com>
2024-01-04 09:14:57 -0700

    Added antd theme

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,17 +5,37 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Planner from "./pages/Planner";
 import Home from "./pages/Home";
+import { ConfigProvider, ThemeConfig } from "antd";
+
+const theme: ThemeConfig = {
+  components: {
+    Input: {
+      colorPrimary: "#f87060",
+      algorithm: true,
+    },
+    Select: {
+      colorPrimary: "#f87060",
+      algorithm: true,
+    },
+    DatePicker: {
+      colorPrimary: "#f87060",
+      algorithm: true,
+    },
+  },
+};
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="planner" element={<Planner />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ConfigProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="planner" element={<Planner />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ConfigProvider>
   );
 }
 
